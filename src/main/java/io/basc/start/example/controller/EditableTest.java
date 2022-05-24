65097eb8df1bf0bb8870f86fac8b2166eb5e1995
wcnnkh <416078075@qq.com>
2022-05-24 13:32:09 +0800

    cleanup

--- src/main/java/io/basc/start/example/controller/EditableTest.java
+++ src/main/java/io/basc/start/example/controller/EditableTest.java
@@ -1,17 +1,24 @@
 package io.basc.start.example.controller;
 
+import io.basc.framework.orm.annotation.Condition;
+import io.basc.framework.orm.annotation.Entity;
 import io.basc.framework.orm.annotation.PrimaryKey;
-import io.basc.start.data.annotation.Editable;
-import io.basc.start.data.annotation.Image;
+import io.basc.framework.orm.annotation.SortType;
+import io.basc.framework.util.comparator.Sort;
+import io.basc.start.editable.annotation.Editable;
+import io.basc.start.editable.annotation.Image;
 import lombok.Data;
 
-@Editable(title = "可编辑测试")
+@Editable
+@Entity(comment = "可编辑测试")
 @Data
 public class EditableTest {
 	@PrimaryKey
 	private String name;
 	@Image
+	@Condition("like")
 	private String img;
 	@Image(multiple = true)
+	@SortType(Sort.DESC)
 	private String imgs;
 }
