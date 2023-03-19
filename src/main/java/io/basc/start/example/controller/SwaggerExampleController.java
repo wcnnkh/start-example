d94fa6b081b0aa3c078257f5fcf9e4297e2b33ee
wcnnkh <247044007@qq.com>
2023-03-19 22:32:50 +0800

    cleanup

--- src/main/java/io/basc/start/example/controller/SwaggerExampleController.java
+++ src/main/java/io/basc/start/example/controller/SwaggerExampleController.java
@@ -5,9 +5,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import io.basc.framework.http.MediaType;
+import io.basc.framework.mvc.annotation.Controller;
 import io.basc.framework.web.message.annotation.RequestBody;
 
 @Path("/swagger")
+@Controller
 public class SwaggerExampleController {
 	@Path("form-params")
 	@POST
